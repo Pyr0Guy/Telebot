97abb8dcf00aca2bec3c02261661a6df8395107c
cpu-err <60135127+cpu-err@users.noreply.github.com>
2023-04-10 14:04:02 +0500

    Add files via upload

--- base.py
+++ base.py
@@ -6,13 +6,13 @@ with sq.connect("Base.db") as con:
 	cur.execute("""CREATE TABLE IF NOT EXISTS users (
 		userid INTEGER NOT NULL,
 		name TEXT NOT NULL,
-		group TEXT NOT NULL, #admin, employee
+		group_ TEXT NOT NULL,
 		progress INTEGER DEFAULT 0
 		)""")
 
 
 	def registerUser(Userid, Name, Group, Progress=0):
-		cur.execute(f"INSERT INTO users (userid, name, group, progress) VALUES({Userid}, {Name}, {Group}, {Progress})")
+		cur.execute(f"INSERT INTO users (userid, name, group_, progress) VALUES({Userid}, {Name}, {Group}, {Progress})")
 		pass
 
-#registerUser(3434, "fdsfs", "employee")
+#registerUser(3434, "fdsfshfghf", "employee")
