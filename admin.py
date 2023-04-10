3aafb3e65836ebbebabbffccdd3bc47ffadd1b93
cpu-err <60135127+cpu-err@users.noreply.github.com>
2023-04-10 13:53:12 +0500

    Add files via upload

--- admin.py
+++ admin.py
@@ -8,3 +8,11 @@ def deleteUserButton(): pass
 def showUserData(): pass
 
 
+def addUser(userid, name, group):
+	registerUser(userid, name, group)
+	return f"""
+	Пользователь зарегестрирован
+	id: {userid}
+	Имя: {name}
+
+	"""
--- base.py
+++ base.py
@@ -4,13 +4,15 @@ with sq.connect("Base.db") as con:
 	cur = con.cursor()
 
 	cur.execute("""CREATE TABLE IF NOT EXISTS users (
-		userid INTEGER PRIMARY KEY AUTOINCREMENT,
+		userid INTEGER NOT NULL,
 		name TEXT NOT NULL,
-		group TEXT DEFAULT NULL, #admin, employee
-		telegramlink TEXT NOT NULL
+		group TEXT NOT NULL, #admin, employee
+		progress INTEGER DEFAULT 0
 		)""")
 
 
-	def registerUser(name, telegramlink):
-		#cur.execute("INSERT INTO users()")
+	def registerUser(Userid, Name, Group, Progress=0):
+		cur.execute(f"INSERT INTO users (userid, name, group, progress) VALUES({Userid}, {Name}, {Group}, {Progress})")
 		pass
+
+#registerUser(3434, "fdsfs", "employee")
